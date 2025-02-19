446da9e49e4d8597734c6863605c1eceb9bb070c
Andrés Rueda <andres.rueda@rappi.com>
2025-02-19 11:34:46 -0500

    last

---
+++ gorutines/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"https://www.google.com",
+		"https://www.facebook.com",
+		"https://www.golang.org",
+		"https://www.amazon.com",
+		"https://www.stackoverflow.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		go checkLink(l, c)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	time.Sleep(5 * time.Second)
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println("Error calling the link")
+		c <- link
+		return
+	}
+	fmt.Println(link, "is okey")
+	c <- link
+
+}
